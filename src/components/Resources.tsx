874e5c283497621c9d4364d9a8856dc4475ad40f
jungai <juworaphol125@gmail.com>
2021-12-11 15:28:26 +0700

    fix solid render only once (is not react style lol)

--- src/components/Resources.tsx
+++ src/components/Resources.tsx
@@ -2,13 +2,16 @@ import type { Component } from 'solid-js';
 import { createResource, createSignal, For, Switch } from 'solid-js';
 
 // like react query, rtk, swr
-const getUsers = async () => (await fetch('https://jsonplaceholder.typicode.com/users')).json();
+// const getUsers = async () => (await fetch('https://sonplaceholder.typicode.com/users')).json();
+const getUsers = async () => {
+  await new Promise(res => setTimeout(res, 4000));
+
+  return (await fetch('https://jsonplaceholder.typicode.com/users')).json();
+};
 
 const Resources: Component = () => {
 
   const [users, { mutate, refetch }] = createResource<any[]>(getUsers);
-
-  if (users.loading) <p>loading ....</p>
 
   {/* return <For each={users()} fallback={'no users'}> */ }
   {/*   {(user) => <p>{user.name}</p>} */ }
@@ -16,9 +19,11 @@ const Resources: Component = () => {
 
   return (
     <div>
-      <For each={users()} fallback={'no users'}>
+      <For each={users()}>
         {(user) => <p>{user.name}</p>}
       </For>
+      {<span>{users.loading && "lodaing ..."}</span>}
+      {<span>{users.error && "error"}</span>}
       <button class="border-2 border-red-400" onClick={refetch}>refetch</button>
       <button class="border-2 border-red-400" onClick={() => {
         mutate([{ name: 'iu' }])
